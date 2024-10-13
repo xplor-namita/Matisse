62cf98554a85404bfbfae15ac5989978a2455fa0
rookie <yingkongshi11@gmail.com>
2024-10-13 20:37:32 +0800

    config publish

--- sample/src/main/java/com/zhihu/matisse/sample/SampleActivity.kt
+++ sample/src/main/java/com/zhihu/matisse/sample/SampleActivity.kt
@@ -23,7 +23,6 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.Build
-import android.os.Build.VERSION.SDK
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
